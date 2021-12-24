7e7a544d67f1f997b11134edfcf715d989874324
Katy Harrod <m.catherine.harrod@gmail.com>
2021-12-24 16:38:40 -0700

    Add orderly spec test for trainer index

--- spec/features/trainers/index_spec.rb
+++ spec/features/trainers/index_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe 'Trainers index' do
   it 'can navigate to the trainer index through welcome page' do
@@ -17,5 +17,18 @@ RSpec.describe 'Trainers index' do
     expect(page).to have_content(@ash.name)
     expect(page).to have_content(@blue.name)
     expect(page).to have_content(@red.name)
+  end
+
+  it 'displays all trainers in order of most recently created' do
+    @ash = Trainer.find_or_create_by!(name: "Ash", age: 10, certified: false)
+    @blue = Trainer.find_or_create_by!(name: "Blue", age: 11, certified: false)
+    @red = Trainer.find_or_create_by!(name: "Red", age: 11, certified: false)
+    @serena = Trainer.create!(name: "Serena", age: 13, certified: false)
+
+
+    visit '/trainers'
+    expect(@ash.name).to appear_before(@blue.name)
+    expect(@ash.name).to appear_before(@red.name)
+    expect(@blue.name).to appear_before(@red.name)
   end
 end
