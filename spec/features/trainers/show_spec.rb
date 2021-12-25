2be00a4dd9a8705553f8353cc2a538adb4fdb781
Katy Harrod <m.catherine.harrod@gmail.com>
2021-12-24 18:14:57 -0700

    Add trainers pokemon count spec test

--- spec/features/trainers/show_spec.rb
+++ spec/features/trainers/show_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Trainer, type: :feature do
     expect(page).to have_content(@ash.age)
     expect(page).to have_content(@ash.certified)
   end
+
+  it 'shows how many pokemon the trainer cares for' do
+    @ash = Trainer.find_or_create_by!(name: "Ash", age: 10, certified: false)
+    @blue = Trainer.find_or_create_by!(name: "Blue", age: 11, certified: false)
+    @red = Trainer.find_or_create_by!(name: "Red", age: 11, certified: false)
+
+    visit "/trainers/#{@red.id}"
+    expect(page).to have_content("Number of Pokemon: 2")
+  end
 end
