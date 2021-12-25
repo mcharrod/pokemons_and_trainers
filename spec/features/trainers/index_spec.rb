29cb101d0f1aa13ab16ebd9582dc1bf5c6785415
Katy Harrod <m.catherine.harrod@gmail.com>
2021-12-24 18:04:31 -0700

    Add test to look for created_at date

--- spec/features/trainers/index_spec.rb
+++ spec/features/trainers/index_spec.rb
@@ -31,5 +31,17 @@ RSpec.describe 'Trainers index' do
     expect(@ash.name).to appear_before(@red.name)
     expect(@blue.name).to appear_before(@red.name)
     expect(@red.name).to appear_before(@serena.name)
+    expect(@serena.name).to appear_before(@cynthia.name)
+  end
+
+  it 'displays when the trainers were created' do
+    @ash = Trainer.find_or_create_by!(name: "Ash", age: 10, certified: false)
+    @blue = Trainer.find_or_create_by!(name: "Blue", age: 11, certified: false)
+    @red = Trainer.find_or_create_by!(name: "Red", age: 11, certified: false)
+    @serena = Trainer.find_or_create_by!(name: "Serena", age: 13, certified: false)
+    @cynthia = Trainer.find_or_create_by!(name: "Cynthia", age: 33, certified: true)
+
+    visit "/trainers"
+    expect(page).to have_content("Added to index on 12/20/2021")
   end
 end
